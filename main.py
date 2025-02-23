d9f3a7008c56bbbc76799c2acaaed47917bee842
PyQuar <firas@LAPTOP-708H394O.>
2025-02-23 11:23:42 +0100

    tenth-commit

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import streamlit as st
 from Fonction import display_notebook_charts
 
 if __name__ == "__main__":
-    st.title("By Ayouuuuuuub and SiFiras")
+    st.title("Projet Statistique par Firas et Ayoub")
     notebook_file = "stat-inf-1.ipynb"
     display_notebook_charts(notebook_file)
